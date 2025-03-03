6ccc91362c7626ea560040e86fe78537f70d6202
christiankaylanUW <ckalviz@uw.edu>
2025-03-02 20:33:33 -0800

    add search functionality to classes page

--- src/components/ClassMain.jsx
+++ src/components/ClassMain.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router';
 
 
 
 export default function ClassMain(props) {
   const classesArray = props.classes;
+  const [inputValue, setInputValue] = useState('');
+
+  let handleChange = (event) => {
+    let newValue = event.target.value
+    setInputValue(newValue);
+  }
+
+  const filteredClasses = classesArray.filter((currClass) => {
+    const currClassLowerCase = currClass.title.toLowerCase();
+    const inputValueLowerCase = inputValue.toLowerCase();
+    return currClassLowerCase.includes(inputValueLowerCase);
+  });
+  console.log(filteredClasses);
+
+
 
   return (
-      <div className="container">
+    <div className="container">
+      <div className="d-flex justify-content-begin mb-3"> 
+      <input type="text" onChange={handleChange} value={inputValue} placeholder="Search by name..." />
+      </div>
         <div className="row">
-          {classesArray.map((currClass) => (
+          {filteredClasses.map((currClass) => (
             <ClassCard classObj={currClass}/>
           ))}
         </div>
-      </div>
+    </div>
   )
 }
 
@@ -32,6 +50,6 @@ function ClassCard(props) {
       </div>
     </div>
   </div>
-
   )
 }
+
