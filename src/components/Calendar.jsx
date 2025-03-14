752083b6e43ddb5bd9e61266ac5077492232b6b4
jameschen0117 <q30104@gmail.com>
2025-03-13 18:06:04 -0700

    Fix View Button in Schedule

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 
 export default function Calendar(props) {
@@ -7,41 +7,41 @@ export default function Calendar(props) {
 
     return (
         <main className="calendar-background">
-        {/* <div className="container mt-4"> */}
-        <h1 className="weekly-title">Your Weekly Schedule</h1>
+            {/* <div className="container mt-4"> */}
+            <h1 className="weekly-title">Your Weekly Schedule</h1>
 
-        <div className="calendar-container">        
-            <div className="calendar">
-                <div className="day"><strong>Sunday</strong></div>
-                <div className="day"><strong>Monday</strong>
-                    {/* CARD HERE */}
-                    <CalendarCard classes={classesArr[0]} />
+            <div className="calendar-container">
+                <div className="calendar">
+                    <div className="day"><strong>Sunday</strong></div>
+                    <div className="day"><strong>Monday</strong>
+                        {/* CARD HERE */}
+                        <CalendarCard classes={classesArr[0]} />
+                    </div>
+                    <div className="day"><strong>Tuesday</strong>
+                        <CalendarCard classes={classesArr[1]} />
+                    </div>
+                    <div className="day"><strong>Wednesday</strong></div>
+                    <div className="day"><strong>Thursday</strong>
+                        <CalendarCard classes={classesArr[2]} />
+                        <CalendarCard classes={classesArr[3]} />
+                    </div>
+                    <div className="day"><strong>Friday</strong></div>
+                    <div className="day"><strong>Saturday</strong></div>
                 </div>
-                <div className="day"><strong>Tuesday</strong>
-                    <CalendarCard classes={classesArr[1]} />
-                </div>
-                <div className="day"><strong>Wednesday</strong></div>
-                <div className="day"><strong>Thursday</strong>
-                    <CalendarCard classes={classesArr[2]} />
-                    <CalendarCard classes={classesArr[3]} />
-                </div>
-                <div className="day"><strong>Friday</strong></div>
-                <div className="day"><strong>Saturday</strong></div>
             </div>
-        </div>
-    </main>
+        </main>
     )
 }
 
 function CalendarCard(props) {
     const currClass = props.classes;
 
-    return(
+    return (
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">Gentle Yoga</h5>
                 <p>{currClass.duration} min | @{currClass.location} | {currClass.date} | {currClass.time} | {currClass.notes}</p>
-                <Link to="../pose" className="btn btn-dark btn-sm">View</Link>
+                <Link to={`/my-classes/${currClass.id}/poses`} className="btn btn-dark btn-sm">View</Link>
             </div>
         </div>
     )
