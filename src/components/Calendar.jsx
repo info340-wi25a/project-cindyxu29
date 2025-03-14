b0563c570835e76ec121e6299cd3727871b65b4a
jameschen0117 <q30104@gmail.com>
2025-03-13 18:29:59 -0700

    Fix Calendar and its CSS

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-
 export default function Calendar(props) {
     const classesArr = props.classes;
 
+    const daysMap = {
+        "Sunday": [],
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": []
+    };
+
+    // Sort classes into correct days
+    classesArr.map(currClass => {
+        const classDate = new Date(currClass.date);
+        const dayName = classDate.toLocaleDateString('en-US', { weekday: 'long' });
+        daysMap[dayName].push(currClass);
+    });
+
     return (
         <main className="calendar-background">
-            {/* <div className="container mt-4"> */}
             <h1 className="weekly-title">Your Weekly Schedule</h1>
 
             <div className="calendar-container">
                 <div className="calendar">
-                    <div className="day"><strong>Sunday</strong></div>
-                    <div className="day"><strong>Monday</strong>
-                        {/* CARD HERE */}
-                        <CalendarCard classes={classesArr[0]} />
-                    </div>
-                    <div className="day"><strong>Tuesday</strong>
-                        <CalendarCard classes={classesArr[1]} />
-                    </div>
-                    <div className="day"><strong>Wednesday</strong></div>
-                    <div className="day"><strong>Thursday</strong>
-                        <CalendarCard classes={classesArr[2]} />
-                        <CalendarCard classes={classesArr[3]} />
-                    </div>
-                    <div className="day"><strong>Friday</strong></div>
-                    <div className="day"><strong>Saturday</strong></div>
+                    {Object.keys(daysMap).map(day => (
+                        <div className="day" key={day}>
+                            <strong>{day}</strong>
+                            {daysMap[day].map((currClass, index) => (
+                                <CalendarCard key={index} currClass={currClass} />
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         </main>
-    )
+    );
 }
 
-function CalendarCard(props) {
-    const currClass = props.classes;
-
+function CalendarCard({ currClass }) {
     return (
         <div className="card">
             <div className="card-body">
-                <h5 className="card-title">Gentle Yoga</h5>
-                <p>{currClass.duration} min | @{currClass.location} | {currClass.date} | {currClass.time} | {currClass.notes}</p>
+                <p className="calendar-card-title">{currClass.title}</p>
+                <p className='calendar-card-content'>{currClass.duration} min | @{currClass.location} | {currClass.date} | {currClass.time} | {currClass.notes}</p>
                 <Link to={`/my-classes/${currClass.id}/poses`} className="btn btn-dark btn-sm">View</Link>
             </div>
         </div>
-    )
+    );
 }
