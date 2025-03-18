05cc93e080fa6dfdd8b7b77243e8986ca48b64ca
jameschen0117 <q30104@gmail.com>
2025-03-17 18:15:53 -0700

    Add form vilidation for adding pose modal

--- src/components/ClassButtons.jsx
+++ src/components/ClassButtons.jsx
@@ -1,3 +1,5 @@
+// Ref: https://react-bootstrap.netlify.app/docs/components/modal/
+// Ref: https://react-bootstrap.netlify.app/docs/forms/validation
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -16,16 +18,38 @@ export default function ClassButtons({ onAddClass }) {
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
   const [notes, setNotes] = useState('');
+  const [validated, setValidated] = useState(false);
 
   function handleClose() {
     setShow(false);
+    resetForm();
   }
-  
+
+  function resetForm() {
+    // Clear input fields
+    setClassName('');
+    setDuration('');
+    setLocation('');
+    setDate('');
+    setTime('');
+    setNotes('');
+    setValidated(false);
+  }
+
   function handleShow() {
     setShow(true);
   }
 
-  function handleSubmit() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      setValidated(true);
+      return;
+    }
+    setValidated(true);
+
     const newClass = {
       id: Date.now(),
       title: className,
@@ -39,10 +63,10 @@ export default function ClassButtons({ onAddClass }) {
     //FIREBASE
     const auth = getAuth();
     const user = auth.currentUser;
-    if(user){
+    if (user) {
       const db = getDatabase();
       const classesRef = ref(db, 'users/' + user.uid + '/classes');
-      
+
       //save uid as property of class
       firebasePush(classesRef, newClass).then((newClassesRef) => {
         firebaseUpdate(newClassesRef, { uid: newClassesRef.key });
@@ -52,13 +76,6 @@ export default function ClassButtons({ onAddClass }) {
     onAddClass(newClass);
     handleClose();
 
-    // Clear input fields
-    setClassName('');
-    setDuration('');
-    setLocation('');
-    setDate('');
-    setTime('');
-    setNotes('');
   }
 
   return (
@@ -70,38 +87,39 @@ export default function ClassButtons({ onAddClass }) {
         <Button className="class-button" variant="dark">Go to Calendar</Button>
       </Link>
 
-
       {/* REACT MODAL */}
-      <div>
-        <Modal show={show} onHide={handleClose} centered="true">
+      <Modal show={show} onHide={handleClose} centered="true">
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Modal.Header closeButton>
             <Modal.Title>Create a new Class</Modal.Title>
           </Modal.Header>
+
           <Modal.Body>
-            <Form>
-              <Form.Group>
-                <Form.Label htmlFor="className">Class Name</Form.Label>
-                <Form.Control type="text" id="className" placeholder="Gental Yoga" 
-                  value={className} 
-                  onChange={(e) => setClassName(e.target.value)}
-                  required />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="className">Duration (min)</Form.Label>
-                <Form.Control type="text" id="duration" placeholder="2" 
-                  value={duration} 
-                  onChange={(e) => setDuration(e.target.value)}
-                  required />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="className">Location</Form.Label>
-                <Form.Control type="text" id="location" placeholder="UW IMA" 
-                  value={location} 
-                  onChange={(e) => setLocation(e.target.value)}
-                  required />
-              </Form.Group>
-              
-              <Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="className">Class Name</Form.Label>
+              <Form.Control type="text" id="className" placeholder="Gental Yoga"
+                value={className}
+                onChange={(e) => setClassName(e.target.value)}
+                required />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="className">Duration (min)</Form.Label>
+              <Form.Control type="text" id="duration" placeholder="2"
+                value={duration}
+                onChange={(e) => setDuration(e.target.value)}
+                required />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="className">Location</Form.Label>
+              <Form.Control type="text" id="location" placeholder="UW IMA"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                required />
+            </Form.Group>
+
+            <Form.Group>
               <Form.Label htmlFor="className">Day of Week</Form.Label>
               <Form.Control as="select" id="dayOfWeek"
                 value={date}
@@ -115,32 +133,35 @@ export default function ClassButtons({ onAddClass }) {
                   </option>
                 ))}
               </Form.Control>
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="className">Start Time</Form.Label>
-                <Form.Control type="time" id="time" placeholder="2:00pm" 
-                  value={time} 
-                  onChange={(e) => setTime(e.target.value)}
-                  required />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="className">Notes</Form.Label>
-                <Form.Control type="text" id="notes" placeholder="Beginning lession for UW students" 
-                  value={notes} 
-                  onChange={(e) => setNotes(e.target.value)} />
-              </Form.Group>
-            </Form>
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="className">Start Time</Form.Label>
+              <Form.Control type="time" id="time" placeholder="2:00pm"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                required />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label htmlFor="className">Notes</Form.Label>
+              <Form.Control type="text" id="notes" placeholder="Beginning lession for UW students"
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)} />
+            </Form.Group>
+
           </Modal.Body>
+
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
-            <Button variant="primary" onClick={handleSubmit}>
+            <Button variant="primary" type="submit">
               Add Class
             </Button>
           </Modal.Footer>
-        </Modal>
-      </div>
+        </Form>
+      </Modal>
     </>
   )
 }
